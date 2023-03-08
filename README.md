69f55a2eaa6066f324ecba6d4420f4c43ad4dbc5
emilhein <emilhein1@gmail.com>
2023-03-08 08:18:02 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Cooie-abuse-client1
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-pgoxmv)
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,35 @@
+<script>
+  let likes = []
+  const ilike = (topic) => {
+    if(!likes.includes(topic)){
+      likes = [...likes, topic]
+      try {
+        
+        fetch(`https://expresssimpler53bqn-fdzy--80.local-credentialless.webcontainer.io/cookie/${topic}`)
+          .then(response => {
+    // process data
+    console.log('got it ', response)
+  })
+  .catch(error => {
+    console.error('Panic at the disco:', error);
+  })
+        }catch (error){
+          console.log(error)
+        }
+
+    }
+  }
+</script>
+
+<button on:click={() => ilike('cats')}>
+  I like cats 
+</button>
+<button on:click={() => ilike('horses')}>
+  I like horses 
+</button>
+<button on:click={() => ilike('dogs')}>
+  I like dogs 
+</button>
+<p>
+{likes}
+</p>
